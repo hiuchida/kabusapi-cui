661b8fc808ae1bbbc8aec94af0aa10912410403a
hiuchida <hiro4uchida@gmail.com>
2023-07-24 11:26:50 +0900

    Add MainConsoleUI (under construction)

---
+++ src/main/java/com/github/hiuchida/kabusapi/cui/MainConsoleUI.java
@@ -0,0 +1,9 @@
+package com.github.hiuchida.kabusapi.cui;
+
+public class MainConsoleUI {
+
+	public static void main(String[] args) {
+		System.out.println("Hello MainConsoleUI    under construction");
+	}
+
+}
---
+++ src/test/java/com/github/hiuchida/kabusapi/cui/MainConsoleUITest.java
@@ -0,0 +1,16 @@
+package com.github.hiuchida.kabusapi.cui;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class MainConsoleUITest {
+
+	@Test
+	public void mainConsoleUITest() {
+		assertTrue(true);
+	}
+
+}
